dd227204063a7e779c4d8a60cc38b23554265222
Ahmad <ahmadt@gmail.com>
2023-09-26 21:44:23 +0300

    .

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\APIController;
+use App\Http\Controllers\DocumentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,9 +16,9 @@ use App\Http\Controllers\APIController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::prefix('v1')->group(function () {
     Route::post('/app/{appId}/{apiId}/{name?}', [APIController::class, 'execute']);

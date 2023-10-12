6e35bdcb5e5890ff6286161c4742ec10b1bbb13e
Ahmad <ahmadt@gmail.com>
2023-10-13 02:23:47 +0300

    error style

--- resources/views/errors/foreign-key-violation.blade.php
+++ resources/views/errors/foreign-key-violation.blade.php
@@ -5,11 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
+    <body class="text-black dark:text-white">
         <div class="p-10 text-2xl text-center font-bold">Erorr in deleting/updating this record!. Make sure no other components (API/App/Collection...etc) are using this record.</div>
-
-        <div>
-            <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style" wire:click="switchMode"><i class="ficon" data-feather="moon"></i></a></li>
-        </div>
     </body>
 </html>
